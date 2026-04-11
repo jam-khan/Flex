deaa03e20a736f7724306b812c5e2276b4408630
jam-khan <jamkhan@connect.hku.hk>
2026-04-12 04:19:56 +0800

    Monad operations addded to help with kvars in expr.

--- LeanFixpoint/Monad.lean
+++ LeanFixpoint/Monad.lean
@@ -20,3 +20,21 @@ def KM.isKVar (id : FVarId) : KM Bool := do
 
 def KM.getKVar? (id : FVarId) : KM (Option KVar) := do
   return (← read).kvars.get? id
+
+def KM.getKVarList : KM (List KVar) := do
+  return (← read).kvars.values
+
+-- checks if expr is κ application
+-- returning κ and args
+def KM.isKApp (e : Expr) : KM (Option (KVar × Array Expr)) := do
+  let fn := e.getAppFn
+  if fn.isFVar then
+    if let some κ ← KM.getKVar? fn.fvarId! then
+      return some (κ, e.getAppArgs)
+  return none
+
+-- Collect all κ-variables referenced in an Expr
+-- Strategy: check each known κ's fvarId against the expression
+def KM.exprKVars (e : Expr) : KM (List KVar) := do
+  let kvars ← KM.getKVarList
+  return kvars.filter fun κ => e.containsFVar κ.fvarId
