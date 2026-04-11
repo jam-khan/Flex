b5cb144045ee11ac043a4b00044d194402dde72b
jam-khan <jamkhan@connect.hku.hk>
2026-04-12 04:19:56 +0800

    Monad operations addded to help with kvars in expr.

--- LeanFixpoint/Core/Macros.lean
+++ LeanFixpoint/Core/Macros.lean
@@ -3,30 +3,36 @@ import LeanFixpoint.Core.Types
 
 open Lean
 
-def elabBaseTy (id : TSyntax `ident) : MacroM (TSyntax `term) := do
-  match id.getId with
-  | `int  => `(BaseTy.int)
-  | `bool => `(BaseTy.bool)
-  | n     => Macro.throwError s!"Unknown base type: {n}, expected `int` or `bool`"
-
 declare_syntax_cat constr
 
 syntax term : constr
-
 syntax:30 constr:31 " ∧ " constr:30 : constr
-
-syntax "∀" ident ":" ident "." term "⇒" constr : constr
-
+syntax "∀" ident ":" ident "," term "⇒" constr : constr
 syntax "[" constr "]" : constr
 
 syntax "c{" constr "}" : term
 
+-- Limitation, only supports types like Int, Bool for macros
+-- and can't write compound types yet.
 macro_rules
   | `(c{ $l ∧ $r })
       =>  `(Constraint.conj c{$l} c{$r})
-  | `(c{ ∀ $x:ident : $b:ident . $hyp:term ⇒ $body:constr })
-      => do let bty ← elabBaseTy b
-            `(Constraint.imp $(quote x.getId) $bty $hyp c{$body})
+  | `(c{ ∀ $x:ident : $ty:ident , $hyp:term ⇒ $body:constr })
+      =>  `(Constraint.imp $(quote x.getId) (Lean.mkConst $(quote ty.getId)) $hyp c{$body})
   | `(c{ $p:term })
       =>  `(Constraint.pred $p)
   | `(c{ [ $e:constr ] }) => `(c{$e})
+
+-- Helpers
+open Lean in
+def mkFVar (n : Name) : Expr := .fvar (FVarId.mk n)
+def mkTrue : Expr := mkConst ``True
+
+-- Examples using c{...} macro
+section Examples
+
+def ex1 : Constraint :=
+  c{ ∀ x : Int, mkTrue ⇒
+      [∀ y : Int, mkTrue ⇒ mkTrue] }
+
+end Examples
