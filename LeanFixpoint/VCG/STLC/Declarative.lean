08e0cef2b9305eff35365f93e1595b1d3fab6433
jam-khan <jamkhan@connect.hku.hk>
2026-05-02 00:13:10 +0800

    Declarative typing added for STLC, and soundness w.r.t declarative typing.

---
+++ LeanFixpoint/VCG/STLC/Declarative.lean
@@ -0,0 +1,42 @@
+import LeanFixpoint.VCG.STLC.Typing
+
+open STLC
+
+/-! # Declarative Refinement Typing for STLC
+
+  A non-bidirectional declarative typing judgement, in the style of
+  LambdaRF / SystemRF from paper `Mechanizing Refinement Types`,
+  restricted to current STLC fragment.
+-/
+
+inductive Hastype : TEnv → Exp → Ty → Prop where
+  -- TVar `Γ(x) = t ⇒ Γ ⊢ x : t`
+  | var {Γ x t} :
+      Γ.lookup x = some t →
+      Hastype Γ (.var x) t
+  -- TCon `integer literal`
+  | const {Γ n} :
+      Hastype Γ (.const n) (prim n)
+  -- TAbs (same-binder)
+  | lam {Γ x e s t} :
+      Hastype ((x, s) :: Γ) e t →
+      Hastype Γ (.lam x e) (.arrow x s t)
+  -- TApp (ANF) `e x`
+  | app {Γ e₁ y x s t} :
+      Hastype Γ e₁ (.arrow x s t) →
+      Hastype Γ (.var y) s        →
+      Hastype Γ (.app e₁ (.var y)) (t.rename x y)
+  -- TLet — synthesize binding, push obligation into body.
+  | letin {Γ x e₁ e₂ s t} :
+      Hastype Γ e₁ s →
+      Hastype ((x, s) :: Γ) e₂ t →
+      Hastype Γ (.letin x e₁ e₂) t
+  -- TAnno
+  | ann {Γ e t} :
+      Hastype Γ e t   →
+      Hastype Γ (.ann e t) t
+  -- TSub
+  | sub {Γ e s t} :
+      Hastype Γ e s →
+      Subtyp Γ s t  →
+      Hastype Γ e t
