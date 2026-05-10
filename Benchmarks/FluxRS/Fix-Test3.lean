02a0d4be6199e87a8b3985eaa7aef1e4eaf99899
jam-khan <jamkhan@connect.hku.hk>
2026-05-10 10:17:17 +0800

    Test added updated gitignore

--- Benchmarks/FluxRS/Fix-Test3.lean
+++ Benchmarks/FluxRS/Fix-Test3.lean
@@ -57,4 +57,8 @@ def CounterLoop : Prop :=
                 k0 (res₀ + 1) (i₀ - 1) n₀))
 
 theorem CounterLoop_proof : CounterLoop := by
-  solve_fixpoint
+  try solve_fixpoint
+
+example : (z0+z1=z2 ∧ z1≥0 ∧ z0≥0) ∧ (z1+z0=z2 ∧ z0≥0 ∧ z1≥0) ∧ z2≥z0 ∧ z2≥z1 ∧ z0≤z2 ∧ z1≤z2 ∧ z0≥0 ∧ z1≥0 ∧ z2≥0 := by
+  simp
+  grind
