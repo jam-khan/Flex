1b19265975fffc557befc34842728652ad0e0dd9
jam-khan <jamkhan@connect.hku.hk>
2026-06-02 23:33:39 +0800

    added UX demo for main tactics and added exports to Tactics.lean

--- LeanFixpoint/Tactic/Tactics.lean
+++ LeanFixpoint/Tactic/Tactics.lean
@@ -3,6 +3,8 @@ import LeanFixpoint.Tactic.Tactics.Hoist
 import LeanFixpoint.Tactic.Tactics.Zap
 import LeanFixpoint.Tactic.Tactics.SplitHyps
 import LeanFixpoint.Tactic.Tactics.RewriteKs
+import LeanFixpoint.Tactic.Tactics.ReorderKs
 import LeanFixpoint.Tactic.Tactics.ZapK
 import LeanFixpoint.Tactic.Tactics.Fusion
+import LeanFixpoint.Tactic.Tactics.Fixpoint
 import LeanFixpoint.Tactic.Tactics.Sol1
