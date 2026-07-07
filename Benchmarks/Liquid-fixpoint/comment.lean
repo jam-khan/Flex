873c629f5c3222e21711c8f477f1b7573aa70792
jam-khan <jamkhan@connect.hku.hk>
2026-07-07 23:54:15 +0800

    some examples fixed

--- Benchmarks/Liquid-fixpoint/comment.lean
+++ Benchmarks/Liquid-fixpoint/comment.lean
@@ -50,11 +50,6 @@ def comment_vc : Prop :=
                   ∀ a13 : Int, k3 a13 a0 a1 →
                     0 ≤ a11 - a0))
 
-
-theorem test : comment_vc := by
-  solve_fixpoint
-
 theorem test' : comment_vc := by
   fusion
-  fixpoint
-  all_goals grind
+  solve_fixpoint
